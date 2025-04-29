84ba5b0010b6d353c675e1a8702947d3c1fca009
Muditha Sasanka Kodikara <121278918+mudithasasanka7@users.noreply.github.com>
2025-04-29 07:54:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My CV</h1>
+      <p><strong>Name:</strong> Muditha Sasanka</p>
+      <p><strong>Email:</strong> muditha@example.com</p>
+      <p><strong>Skills:</strong> React, Node.js, PHP, Laravel</p>
     </div>
   );
 }
