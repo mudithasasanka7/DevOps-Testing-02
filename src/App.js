ed8fd955db40491b4f4d899b694b624269d1fb57
Muditha Sasanka Kodikara <121278918+mudithasasanka7@users.noreply.github.com>
2025-04-30 11:11:02 +0530

    Persanal Info

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <p><strong>Name:</strong> Muditha Sasanka Kodikara</p>
       <p><strong>Email:</strong> muditha@gmail.com</p>
       <p><strong>Contact:</strong> 011-1234568</p>
+      <p><strong>Personal:</strong> 0775097307</p>
       <p><strong>Skills:</strong> React, Node.js, PHP, Laravel, Cloud, Java, Python</p>
       <p><strong>Languages:</strong> Sinhala, English, Tamil, Hindi, Japan, Fanish</p>
     </div>
